044e9d69ac699ecac2c3b059785092ca74fe5517
Dickson Tse <dtse87@gmail.com>
2012-11-20 23:35:37 -0800

    Add functions to parse and display data from the HNSearch RSS feed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +14,57 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using QDFeedParser;
 
 namespace HNTiles
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        TilesCollection _tiles = new TilesCollection();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.TilesList.ItemsSource = _tiles;
+        }
+
+        private void GetTiles(object sender, RoutedEventArgs e)
+        {
+            btnGetTiles.IsEnabled = false;
+
+            Uri feeduri = new Uri("http://www.hnsearch.com/bigrss");
+            IFeedFactory factory = new HttpFeedFactory();
+            IFeed feed = factory.CreateFeed(feeduri);
+            foreach (var item in feed.Items)
+            {
+                Tile t = new Tile();
+                t.Title = item.Title;
+                t.Link = item.Link;
+                _tiles.Add(t);
+            }
+
+            btnGetTiles.IsEnabled = true;
+        }
+    }
+
+    class Tile
+    {
+        public string Title { get; set; }
+        public string Link { get; set; }
+    }
+
+    class TilesCollection :  ObservableCollection<Tile>
+    {
+        public void CopyFrom(IEnumerable<Tile> tiles)
+        {
+            this.Items.Clear();
+            foreach (var t in tiles)
+            {
+                this.Items.Add(t);
+            }
+
+            this.OnCollectionChanged(new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
         }
     }
 }
